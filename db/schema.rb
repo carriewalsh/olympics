da4e36b070ffb61a3d8096c673545e659fa49f02
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-28 17:42:09 -0600

    Migrates medal table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_232741) do
+ActiveRecord::Schema.define(version: 2019_07_28_234137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "medals", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "raws", force: :cascade do |t|
     t.string "name"
