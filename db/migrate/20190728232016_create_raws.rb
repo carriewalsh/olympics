ad7c81a91315564f1d045475bd99ddb77e48562d
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-28 17:20:58 -0600

    Migrates raw table

---
+++ db/migrate/20190728232016_create_raws.rb
@@ -0,0 +1,17 @@
+class CreateRaws < ActiveRecord::Migration[5.2]
+  def change
+    create_table :raws do |t|
+      t.string :name
+      t.string :sex
+      t.integer :height
+      t.integer :weight
+      t.string :team
+      t.string :games
+      t.string :sport
+      t.string :event
+      t.string :medal
+
+      t.timestamps
+    end
+  end
+end
