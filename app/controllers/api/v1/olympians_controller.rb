23b38bb0ed06f713b776056ee02ba46a35d26e28
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-29 15:33:12 -0600

    Adds oldest/youngest spec to olympians request and returns array of athletes that age

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::OlympiansController < ApplicationController
   def index
-    olympians = Olympian.list_olympians
+    if params[:age] == "youngest"
+      age = Olympian.order(age: :asc).first.age
+      olympians = Olympian.where(age: age).list_olympians
+    elsif params[:age] == "oldest"
+      age = Olympian.order(age: :desc).first.age
+      olympians = Olympian.where(age: age).list_olympians
+    else
+      olympians = Olympian.list_olympians
+    end
     render json: olympians
   end
 end
