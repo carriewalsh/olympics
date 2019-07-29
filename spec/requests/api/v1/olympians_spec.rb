eb54868b45afc2e35b2dcd5a3dbf11d43ea1c2c1
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-29 15:19:07 -0600

    Finishes request spec for '/olympians'

--- spec/requests/api/v1/olympians_spec.rb
+++ spec/requests/api/v1/olympians_spec.rb
@@ -5,58 +5,42 @@ require "rails_helper"
 describe "As a visitor" do
   describe "when I reach olympian_stats" do
     it "returns the stats for that game's olympians" do
+      OlympianEvent.destroy_all
+      Event.destroy_all
       Olympian.destroy_all
+      Sport.destroy_all
+      Medal.destroy_all
       Team.destroy_all
       @team = Team.create!(name: "USA")
       @o1 = @team.olympians.create(name: "Name 1", sex: "F", age: 18, weight: 80, height: 140)
       @o2 = @team.olympians.create(name: "Name 1", sex: "F", age: 36, weight: 100, height: 150)
       @o3 = @team.olympians.create(name: "Name 1", sex: "M", age: 20, weight: 100, height: 160)
       @o4 = @team.olympians.create(name: "Name 1", sex: "M", age: 30, weight: 120, height: 170)
+      @sport = Sport.create(name: "Taekwondo")
+      @event1 = @sport.events.create(name:"t1")
+      @event2 = @sport.events.create(name:"t2")
+      @event3 = @sport.events.create(name:"t3")
+      @gold = Medal.create(name: "Gold", icon: "asdf")
+      @silver = Medal.create(name: "Silver", icon: "asdf")
+      @oe1 = OlympianEvent.create(olympian_id: @o1.id, event_id: @event1.id, medal_id: @gold.id)
+      @oe2 = OlympianEvent.create(olympian_id: @o1.id, event_id: @event2.id, medal_id: @silver.id)
+      @oe3 = OlympianEvent.create(olympian_id: @o1.id, event_id: @event3.id)
+      @oe4 = OlympianEvent.create(olympian_id: @o2.id, event_id: @event3.id, medal_id: @silver.id)
+      @oe5 = OlympianEvent.create(olympian_id: @o3.id, event_id: @event3.id, medal_id: @silver.id)
+      @oe6 = OlympianEvent.create(olympian_id: @o4.id, event_id: @event3.id, medal_id: @silver.id)
 
 
       get "/api/v1/olympians"
       expect(response.status).to eq(200)
 
       body = JSON.parse(response.body, symbolize_names: true)
-      output = {
-                  "olympian_stats": {
-                    "total_competing_olympians": 4,
-                    "average_weight": {
-                      "unit": "kg",
-                      "male_olympians": 110.0,
-                      "female_olympians": 90.0
-                    },
-                    "average_height": {
-                      "unit": "cm",
-                      "male_olympians": 165.0,
-                      "female_olympians": 145.0
-                    },
-                    "average_age": 26.0
-                  }
-                }
-  # //Response Format
-  # {
-  #   "olympians":
-  #     [
-  #       {
-  #         "name": "Maha Abdalsalam",
-  #         "team": "Egypt",
-  #         "age": 18,
-  #         "sport": "Diving"
-  #         "total_medals_won": 0
-  #       },
-  #       {
-  #         "name": "Ahmad Abughaush",
-  #         "team": "Jordan",
-  #         "age": 20,
-  #         "sport": "Taekwondo"
-  #         "total_medals_won": 1
-  #       },
-  #       {...}
-  #     ]
-  # }
 
-
+      expect(body[:olympians].count).to eq(4)
+      expect(body[:olympians].first[:name]).to eq("Name 1")
+      expect(body[:olympians].first[:team]).to eq("USA")
+      expect(body[:olympians].first[:age]).to eq(18)
+      expect(body[:olympians].first[:sport]).to eq("Taekwondo")
+      expect(body[:olympians].first[:total_medals_won]).to eq(2)
     end
   end
 end
