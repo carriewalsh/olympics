1fb393ca48db76f3dbf834762fe7616024d20238
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-29 12:08:07 -0600

    Namespaces api/v1 and adds get olympians route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/api/v1/olympian_stats", to: "stats#show"
+  namespace :api do
+    namespace :v1 do
+      get "/olympian_stats", to: "stats#show"
+
+      get "/olympians", to: "olympians#index"
+    end
+  end
 end
