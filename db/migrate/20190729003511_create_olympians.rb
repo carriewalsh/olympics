60095f6e43717ad49dde44ebfc57936956fa5290
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-28 18:35:57 -0600

    Migrates olypians table - logic elsewhere will delete repeats - name does not need to be unique

---
+++ db/migrate/20190729003511_create_olympians.rb
@@ -0,0 +1,14 @@
+class CreateOlympians < ActiveRecord::Migration[5.2]
+  def change
+    create_table :olympians do |t|
+      t.string :name
+      t.string :sex
+      t.integer :age
+      t.integer :weight
+      t.integer :height
+      t.references :team, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_002936) do
+ActiveRecord::Schema.define(version: 2019_07_29_003511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.bigint "sport_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sport_id"], name: "index_events_on_sport_id"
+  end
 
   create_table "medals", force: :cascade do |t|
     t.string "name"
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "olympians", force: :cascade do |t|
+    t.string "name"
+    t.string "sex"
+    t.integer "age"
+    t.integer "weight"
+    t.integer "height"
+    t.bigint "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_olympians_on_team_id"
   end
 
   create_table "raws", force: :cascade do |t|
@@ -49,4 +69,6 @@ ActiveRecord::Schema.define(version: 2019_07_29_002936) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "events", "sports"
+  add_foreign_key "olympians", "teams"
 end
