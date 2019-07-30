b1c4aae4ab67e930f0922cf94d0dd393bc317b35
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-29 18:49:27 -0600

    Adds spec for olympian event summary method

--- spec/models/olympian_event_spec.rb
+++ spec/models/olympian_event_spec.rb
@@ -5,4 +5,32 @@ RSpec.describe OlympianEvent, type: :model do
     it { should belong_to :olympian }
     it { should belong_to :event }
   end
+
+  describe "instance methods" do
+    describe "summary" do
+      it "returns the summary of a medalist" do
+        OlympianEvent.destroy_all
+        Event.destroy_all
+        Olympian.destroy_all
+        Sport.destroy_all
+        Medal.destroy_all
+        Team.destroy_all
+        team = Team.create(name: "USA")
+        o1 = team.olympians.create(name: "Name 1", sex: "F", age: 18, weight: 80, height: 140)
+        sport = Sport.create(name: "Taekwondo")
+        event1 = sport.events.create(name:"t1")
+        gold = Medal.create(name: "Gold", icon: "asdf")
+        oe1 = OlympianEvent.create(olympian_id: o1.id, event_id: event1.id, medal_id: gold.id)
+
+        output = {
+          "name": "Name 1",
+          "team": "USA",
+          "age": 18,
+          "medal": "Gold"
+        }
+
+        expect(oe1.summary).to eq(output)
+      end
+    end
+  end
 end
