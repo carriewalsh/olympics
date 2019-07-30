105606cbdd797aa022606eab46d6e3dfb10fdb49
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-29 19:32:55 -0600

    Adds olympians_list method to teams to list all members of all teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,17 @@ class Team < ApplicationRecord
   validates_presence_of :name
 
   has_many :olympians
+
+  def self.olympians_list
+    teams = Team.all.map do |team|
+      olys = team.olympians.map {|olympian| olympian.name}
+      {
+        "team": team.name,
+        "olympians": olys
+      }
+    end
+    {
+      "teams": teams
+    }
+  end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -12,63 +12,68 @@ RSpec.describe Team, type: :model do
 
   describe "class methods" do
     describe "olympians_list" do
-      OlympianEvent.destroy_all
-      Event.destroy_all
-      Olympian.destroy_all
-      Sport.destroy_all
-      Medal.destroy_all
-      Team.destroy_all
-      team = Team.create(name: "USA")
-      o1 = team.olympians.create(name: "Name 1", sex: "F", age: 18, weight: 80, height: 140)
-      o2 = team.olympians.create(name: "Name 2", sex: "F", age: 18, weight: 80, height: 140)
+      it "returns a list of all teams with olympians" do
+        OlympianEvent.destroy_all
+        Event.destroy_all
+        Olympian.destroy_all
+        Sport.destroy_all
+        Medal.destroy_all
+        Team.destroy_all
+        team = Team.create(name: "USA")
+        o1 = team.olympians.create(name: "Name 1", sex: "F", age: 18, weight: 80, height: 140)
+        o2 = team.olympians.create(name: "Name 2", sex: "F", age: 18, weight: 80, height: 140)
 
-      output = {
-        "countries": [
-          {
-            "country": "USA",
-            "olympians":
-            [o1.name,o2.name]
-          }
-        ]
-      }
+        output = {
+          "teams": [
+            {
+              "team": "USA",
+              "olympians":
+              [o1.name,o2.name]
+            }
+          ]
+        }
 
-      expect(Team.olympians_list).to eq(output)
+        expect(Team.olympians_list).to eq(output)
+      end
     end
   end
 
   describe "instance methods" do
+    describe ""
     describe "medalists" do
-      OlympianEvent.destroy_all
-      Event.destroy_all
-      Olympian.destroy_all
-      Sport.destroy_all
-      Medal.destroy_all
-      Team.destroy_all
-      team = Team.create(name: "USA")
-      o1 = team.olympians.create(name: "Name 1", sex: "F", age: 18, weight: 80, height: 140)
-      o2 = team.olympians.create(name: "Name 2", sex: "F", age: 18, weight: 80, height: 140)
-      sport = Sport.create(name: "Taekwondo")
-      event1 = sport.events.create(name:"t1")
-      gold = Medal.create(name: "Gold", icon: "asdf")
-      silver = Medal.create(name: "Silver", icon: "asdf")
-      oe1 = OlympianEvent.create(olympian_id: o1.id, event_id: event1.id, medal_id: gold.id)
-      oe2 = OlympianEvent.create(olympian_id: o2.id, event_id: event1.id, medal_id: silver.id)
+      it "returns the medalist summaries for a team" do
+        OlympianEvent.destroy_all
+        Event.destroy_all
+        Olympian.destroy_all
+        Sport.destroy_all
+        Medal.destroy_all
+        Team.destroy_all
+        team = Team.create(name: "USA")
+        o1 = team.olympians.create(name: "Name 1", sex: "F", age: 18, weight: 80, height: 140)
+        o2 = team.olympians.create(name: "Name 2", sex: "F", age: 18, weight: 80, height: 140)
+        sport = Sport.create(name: "Taekwondo")
+        event1 = sport.events.create(name:"t1")
+        gold = Medal.create(name: "Gold", icon: "asdf")
+        silver = Medal.create(name: "Silver", icon: "asdf")
+        oe1 = OlympianEvent.create(olympian_id: o1.id, event_id: event1.id, medal_id: gold.id)
+        oe2 = OlympianEvent.create(olympian_id: o2.id, event_id: event1.id, medal_id: silver.id)
 
-      output1 = {
-        "name": "Name 1",
-        "team": "USA",
-        "age": 18,
-        "medal": "Gold"
-      }
-      output2 = {
-        "name": "Name 2",
-        "team": "USA",
-        "age": 18,
-        "medal": "Silver"
-      }
+        output1 = {
+          "name": "Name 1",
+          "team": "USA",
+          "age": 18,
+          "medal": "Gold"
+        }
+        output2 = {
+          "name": "Name 2",
+          "team": "USA",
+          "age": 18,
+          "medal": "Silver"
+        }
 
-      expect(team.medalists[:medalists].first).to eq(output1)
-      expect(team.medalists[:medalists].last).to eq(output2)
+        expect(team.medalists[:medalists].first).to eq(output1)
+        expect(team.medalists[:medalists].last).to eq(output2)
+      end
     end
   end
 end
